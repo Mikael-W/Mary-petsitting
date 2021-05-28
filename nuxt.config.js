d86d4c62e0c7f28c1bc79bafdbd35d6e3f6590c9
mikael wawrziczny <mikaelwaw@gmail.com>
2021-05-28 17:15:20 +0200

    favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -24,7 +24,7 @@ export default {
       { hid: 'description', name: 'description', content: '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/mary_favicon.png' }
     ]
   },
 
