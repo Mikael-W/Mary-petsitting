d30991c2f38ee2c82d2ac816ced1d17eb92088ce
mikael wawrziczny <mikaelwaw@gmail.com>
2021-05-28 16:25:06 +0200

    deploy fixing

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,11 +7,14 @@ export default {
     CONTENTFUL_ENVIRONMENT: process.env.CTF_ENVIRONMENT
   },
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
-  ssr: true,
+  ssr: false,
 
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
-
+  // Correction netlify 404
+  generate: {
+    fallback: true
+  },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'mary',
