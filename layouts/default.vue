f9b694f430b190c2e3800938ef8f12f95d2b2c50
Mikael-W <mikaelwaw@gmail.com>
2022-12-26 09:51:09 +0100

    font layout fixed

--- layouts/default.vue
+++ layouts/default.vue
@@ -52,6 +52,6 @@ table {
 @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap');
 @font-face {
   font-family: 'PumpkinStory';
-  src: url('./assets/font/PumpkinStory.ttf'); 
+  src: url('../assets/font/PumpkinStory.ttf'); 
 }
 </style>
